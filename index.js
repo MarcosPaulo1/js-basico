c174a2884664e23751964636cd64a26f30089bc2
Marcos Paulo <marcospaulo.da@gmail.com>
2020-03-10 15:24:46 -0300

    lista de alunos

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-console.log("hello world");
 
-console.log(2+2);
+let idade = 45;
+
+
+let aluno1 = {
+    nome: "Roger",
+    idade: 21,
+    curso: "js"
+}
+let aluno2 = {
+    nome: "Carlsen",
+    idade: idade,
+    curso: "xadrez"
+}
+let lista = [aluno1, aluno2];
+
+for (let aluno of lista){
+    if (aluno.idade < 50){
+        console.log(aluno);
+    }
+}
